9aa93d5533e1b43cfe12609c8b4fe9a18d6d5458
lishka90 <120580693+lishka90@users.noreply.github.com>
2023-01-19 15:38:17 +0300

    5

---
+++ sem4/Program.cs
@@ -0,0 +1,47 @@
+﻿/*
+//Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+Console.WriteLine("введите число A");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("введите число B");
+int b = Convert.ToInt32(Console.ReadLine());
+int step = a;
+
+for (int i = 1; i < b; i++)
+{
+step = step * a;
+}
+Console.WriteLine("A в степени B равно: " + step);
+*/
+/*
+// Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+Console.WriteLine("введите число");
+int i = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+
+while (i > 0)
+{
+int num = i % 10;
+i = i / 10;
+sum = sum + num;
+}
+Console.WriteLine("сумма всех цифр в числе равна: " + sum);
+*/
+/*
+//Напишите программу, которая задаёт массив из m элементов и выводит их на экран.
+int lenArray = ReadInt("Введите длинну массива: ");
+
+int[] randomArray = new int[lenArray];
+for (int i = 0; i < randomArray.Length; i++)
+{
+randomArray[i] = new Random().Next(1,9);
+Console.Write(randomArray[i] + " ");
+}
+
+
+// Функция ввода
+int ReadInt(string message)
+{
+Console.Write(message);
+return Convert.ToInt32(Console.ReadLine());
+}
+*/
